a76c49bd842083dbdf765fd882f9d2d8986f4ab8
sahua3 <148651919+sahua3@users.noreply.github.com>
2023-10-23 14:34:58 -0400

    Created test one

---
+++ myClass.java
@@ -0,0 +1,7 @@
+public class myClass{
+    public void doingMath(int x, int y){
+        System.void.println("Your first number: " + x);
+        System.void.println("Your second number: " + y);
+        System.void.println("x + y = " + (x+y));
+    }
+}
---
+++ myFirstTest.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class myFirstClass{
+    public static void main(String args[]){
+        Scanner myscan = new Scanner();
+        System.out.println("enter your first int.");
+        int x = myscan.nextInt();
+        System.out.println(x);
+    }
+}
